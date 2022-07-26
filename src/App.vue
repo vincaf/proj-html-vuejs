e41879457c33821797275cb1d933f20c4b5f5042
vincaf <vincenzocafassog@gmail.com>
2022-07-26 16:25:16 +0200

    bootstrap installed and imported

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
