3630d6606ff422ae70fcc8ebc9718871a45ea6d0
vincaf <vincenzocafassog@gmail.com>
2022-07-27 12:06:03 +0200

    feat(main): add boxes for article categories

---
+++ src/components/ArticleCategories.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <div class="container">
+        <div class="d-flex justify-content-around py-5">
+            <span>GADGETS</span>
+            <span>PHOTOGRAPHY</span>
+            <span>LIFESTYLE</span>
+            <span>FASHION</span>
+            <span>RECIPES</span>
+            <span>TRAVEL</span>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+section{
+    background-color: $blue_darker;
+
+    span{
+        background-color: $light_black;
+        color: #fff;
+        padding: 15px 30px;
+        font-size: 10px;
+        font-weight: bold;
+        width: 150px;
+        text-align: center;
+    }
+}
+</style>
--- src/components/MainWeb.vue
+++ src/components/MainWeb.vue
@@ -2,18 +2,21 @@
   <section>
     <FeaturedArticles />
     <PostsList />
+    <ArticleCategories />
   </section>
 </template>
 
 <script>
 import FeaturedArticles from './FeaturedArticles.vue';
 import PostsList from './PostsList.vue';
+import ArticleCategories from './ArticleCategories.vue';
 
 export default {
   name: 'MainWeb',
   components: {
     FeaturedArticles,
     PostsList,
+    ArticleCategories,
   }
 }
 </script>
