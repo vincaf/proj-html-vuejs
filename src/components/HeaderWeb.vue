25f080918baaa234d11e21657ac647b777feb814
vincaf <vincenzocafassog@gmail.com>
2022-07-27 02:11:21 +0200

    feat: add nav link

--- src/components/HeaderWeb.vue
+++ src/components/HeaderWeb.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="header_menu d-flex">
         <div class="container">
-            <ul class="d-flex list-unstyled mb-0 py-3 align-items-center">
+            <ul class="d-flex list-unstyled mb-0 py-3">
                 <li class="slogan">Everything about Lifestyle, Travel and Gadgets!</li>
                 <li>SIGN IN</li>
                 <li>ABOUT US</li>
@@ -14,6 +14,27 @@
     <div class="logo py-4">
         <div class="container">
             <img src="../assets/images/logo-default-slim.png" alt="logo">
+        </div>
+    </div>
+    <hr class="m-0">
+    <div class="nav">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <ul class="d-flex list-unstyled mb-0 py-3">
+                        <li class="active">HOME</li>
+                        <li>ELEMENTS</li>
+                        <li>FEATURES</li>
+                        <li>PAGES</li>
+                        <li>PORTFOLIO</li>
+                        <li>BLOG</li>
+                        <li>SHOP</li>
+                    </ul>
+                </div>
+                <div>
+                    <i class="fa-solid fa-magnifying-glass py-3"></i>
+                </div>
+            </div>
         </div>
     </div>
   </section>
@@ -33,14 +54,14 @@ export default {
     color: #fff;
 
     ul{
-        .slogan{
-            opacity: .7;
-        }
-
         li{
             margin-right: 10px;
             font-size: 10px;
             opacity: .9;
+
+            &.slogan{
+                opacity: .7;
+            }
         }
     }
 }
@@ -48,6 +69,26 @@ export default {
 .logo{
     img{
         width: 7%;
+    }
+}
+
+hr{
+    opacity: .1;
+}
+
+.nav{
+    color: $grey_nav;
+
+    ul{
+        li{
+            margin-right: 30px;
+            font-size: 13px;
+            font-weight: bold;
+
+            &.active{
+                color: $blue_primary;
+            }
+        }
     }
 }
 </style>
