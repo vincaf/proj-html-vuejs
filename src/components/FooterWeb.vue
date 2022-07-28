70761977f06e47022a39defc62c26cc744ad5984
vincaf <vincenzocafassog@gmail.com>
2022-07-28 02:03:43 +0200

    feat(Footer): add footer_low /full static layout

--- src/components/FooterWeb.vue
+++ src/components/FooterWeb.vue
@@ -72,7 +72,27 @@
         </div>
     </div>
     <div id="footer_low">
+        <div class="container">
+            <div class="d-flex py-3 justify-content-between">
+                <div class="d-flex align-items-center">
+                    <img src="../assets/images/logo-footer.png" alt="">
+                    <div class="ms-5">© Copyright 2022. All Rights Reserved</div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="fw-bold click">
+                        <i class="fa-solid fa-angle-right me-1"></i> FAQ's
+                    </div>
+                    <div class="ms-3 fw-bold click">
+                        <i class="fa-solid fa-angle-right me-1"></i> Sitemap
+                    </div>
+                    <div class="ms-3 fw-bold click">
+                        <i class="fa-solid fa-angle-right me-1"></i> Contact Us
+                    </div>
+                </div>
+            </div>
 
+            <a href="#"><i class="fa-solid fa-angle-up"></i></a>
+        </div>
     </div>
   </section>
 </template>
@@ -152,5 +172,30 @@ export default {
 
 #footer_low{
     background-color: $bg_footer_low;
+    font-size: 10px;
+    color: $grey_nav;
+
+    .container{
+        position: relative;
+    }
+
+    img{
+        width: 15%;
+        opacity: .5;
+    }
+
+    a{
+        background-color: #444;
+        color: #fff;
+        font-size: 9px;
+        padding: 8px 13px;
+        position: absolute;
+        right: -140px;
+        bottom: 0;
+    }
+
+    .click{
+        cursor: pointer;
+    }
 }
 </style>
