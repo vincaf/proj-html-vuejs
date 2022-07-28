1db23bf6f9e137039c20b726e39fefa054a6a2d3
vincaf <vincenzocafassog@gmail.com>
2022-07-28 12:44:41 +0200

    feat(BoxArticles): add articles with v-for cycle

---
+++ src/components/Main/BoxArticles.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="box d-flex flex-column justify-content-end">
+        <img :src="imgUrl" :alt="title">
+        <span class="category">
+            {{ category }}
+        </span>
+        <div class="title">
+            {{ title }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        'category': String,
+        'title': String,
+        'imgUrl': String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/variables.scss";
+
+.box{
+    background-color: #000;
+    height: 200px;
+    width: 375px;
+    padding: 20px;
+    margin-bottom: 30px;
+    position: relative;
+
+    img{
+        height: 200px;
+        width: 375px;
+        left: 0;
+        top: 0;
+        opacity: .7;
+        position: absolute;
+    }
+
+    .category{
+        z-index: 1;
+        font-size: 10px;
+        width: 90px;
+        padding: 5px;
+        font-weight: bold;
+        text-align: center;
+        color: #fff;
+        background-color: $blue_primary;
+    }
+
+    .title{
+        z-index: 1;
+        color: #fff;
+        font-weight: bold;
+        line-height: 18px;
+        margin-top: 8px;
+    }
+}
+</style>
--- src/components/Main/FeaturedArticles.vue
+++ src/components/Main/FeaturedArticles.vue
@@ -2,67 +2,72 @@
   <section>
     <div class="container">
         <div class="d-flex flex-wrap justify-content-between pt-5 pb-3">
-            <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
-
-            <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
-
-            <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
-
-                        <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
-
-            <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
-
-            <div class="box d-flex flex-column justify-content-end">
-                <span class="category">
-                    PHOTOGRAPHY
-                </span>
-                <div class="title">
-                    How To Take Better Concert Pictures in 30 Seconds
-                </div>
-            </div>
+            <BoxArticles v-for="article in articles" :key="article.id"
+            :category="article.category"
+            :title="article.title"
+            :imgUrl="article.img" />
         </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {
+import BoxArticles from './BoxArticles.vue';
 
+export default {
+    name: 'FeaturedArticles',
+
+    components: {
+    BoxArticles,
+    },
+
+    data(){
+        return{
+            articles: [
+                {
+                    id: 1,
+                    category: "PHOTOGRAPHY",
+                    title: "How To Take Better Concert Pictures in 30 Seconds",
+                    img: require("@/assets/images/blog-46.jpg"),
+                },
+
+                {
+                    id: 2,
+                    category: "GADGETS",
+                    title: "Gadgets That Make Your Smarphone Even Smarter",
+                    img: require("@/assets/images/blog-47.jpg"),
+                },
+
+                {
+                    id: 3,
+                    category: "TRAVEL",
+                    title: "20 Top-Rated Tourist Attractions in Manhattan",
+                    img: require("@/assets/images/blog-48.jpg"),
+                },
+
+                {
+                    id: 4,
+                    category: "LIFESTYLE",
+                    title: "The Best Way to Ride a Motorcycle",
+                    img: require("@/assets/images/blog-49.jpg"),
+                },
+
+                {
+                    id: 5,
+                    category: "TRAVEL",
+                    title: "5 Fun Things to Do at the Beach",
+                    img: require("@/assets/images/blog-50.jpg"),
+                },
+
+                {
+                    id: 6,
+                    category: "RECIPES",
+                    title: "Amazingly Fresh Fruit And Herb Drinks For Summer",
+                    img: require("@/assets/images/blog-51.jpg"),
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -71,30 +76,5 @@ export default {
 
 section{
     background-color: $bg_light_grey;
-
-    .box{
-        background-image: url("../../assets/images/blog-46.jpg");
-        background-size: cover;
-        height: 200px;
-        width: 375px;
-        padding: 20px;
-        margin-bottom: 30px;
-
-        .category{
-            font-size: 10px;
-            width: 90px;
-            padding: 5px;
-            font-weight: bold;
-            color: #fff;
-            background-color: $blue_primary;
-        }
-
-        .title{
-            color: #fff;
-            font-weight: bold;
-            line-height: 18px;
-            margin-top: 8px;
-        }
-    }
 }
 </style>
