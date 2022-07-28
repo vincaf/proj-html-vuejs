e41d970da0bbac5a711e26d369633d755e390b0f
vincaf <vincenzocafassog@gmail.com>
2022-07-28 15:13:20 +0200

    refactor(FooterWeb): split in two components

--- src/components/Footer/FooterWeb.vue
+++ src/components/Footer/FooterWeb.vue
@@ -5,101 +5,22 @@
             Get in Touch
         </div>
     </div>
-    <div id="footer_top">
-        <div class="container">
-            <div class="row justify-content-between py-5">
-                <div class="col-4">
-                    <div class="title">ABOUT THE BLOG</div>
-                    <p class="subtitle">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, eligendi eveniet corrupti explicabo molestias qui id dolorem, corporis quae excepturi laborum quidem. Repellendus impedit quae eos, ex aspernatur omnis repudiandae!</p>
-                    <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias expedita aspernatur nam provident maiores, delectus facilis voluptatum aliquam voluptates, sapiente vero necessitatibus at ratione eos natus modi hic unde minima.</p>
-                    <div class="title click">VIEW MORE <i class="fa-solid fa-angle-right"></i></div>
-                </div>
-
-                <div class="col-3">
-                    <div class="title">RECENT POSTS</div>
-                    <div class="d-flex">
-                        <img class="click" src="../../assets/images/our-office-4-square.jpg" alt="recent-post-1">
-                        <div class="ms-2">
-                            <p class="mb-1 click">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            <p class="subtitle">12:53 AM Dec 19th</p>
-                        </div>
-                    </div>
-                    <div class="d-flex">
-                        <img class="click" src="../../assets/images/our-office-5-square.jpg" alt="recent-post-1">
-                        <div class="ms-2">
-                            <p class="mb-1 click">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            <p class="subtitle">12:53 AM Dec 19th</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-3">
-                    <div class="title">RECENT COMMENTS</div>
-                    <div class="comment">
-                        <div>
-                            <i class="fa-solid fa-angle-right active"></i><span class="fw-bold ms-1">John Doe</span><span class="ms-1">commented on</span><span class="ms-1 active">lorem ipsum dolor sit amet.</span>
-                        </div>
-                        <p class="subtitle">12:53 AM Dec 19th</p>
-                    </div>
-
-                    <div class="comment">
-                        <div>
-                            <i class="fa-solid fa-angle-right active"></i><span class="fw-bold ms-1">John Doe</span><span class="ms-1">commented on</span><span class="ms-1 active">lorem ipsum dolor sit amet.</span>
-                        </div>
-                        <p class="subtitle">12:53 AM Dec 19th</p>
-                    </div>
-                </div>
-
-                <div class="col-2">
-                    <div class="title">CATEGORIES</div>
-                    <div class="d-flex flex-wrap">
-                        <span class="tags">GADGETS</span>
-                        <span class="tags">PHOTOGRAPHY</span>
-                        <span class="tags">LIFESTYLE</span>
-                        <span class="tags">FASHION</span>
-                        <span class="tags">RECIPES</span>
-                        <span class="tags">TRAVEL</span>
-                        <span class="tags">BUSINESS</span>
-                        <span class="tags">ARCHITECTURE</span>
-                        <span class="tags">REVIEWS</span>
-                        <span class="tags">SPORTS</span>
-                        <span class="tags">VIDEOS</span>
-                        <span class="tags">TECHNOLOGY</span>
-                        <span class="tags">DESIGN</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="footer_low">
-        <div class="container">
-            <div class="d-flex py-3 justify-content-between">
-                <div class="d-flex align-items-center">
-                    <img src="../../assets/images/logo-footer.png" alt="">
-                    <div class="ms-5">© Copyright 2022. All Rights Reserved</div>
-                </div>
-                <div class="d-flex align-items-center">
-                    <div class="fw-bold click">
-                        <i class="fa-solid fa-angle-right me-1"></i> FAQ's
-                    </div>
-                    <div class="ms-3 fw-bold click">
-                        <i class="fa-solid fa-angle-right me-1"></i> Sitemap
-                    </div>
-                    <div class="ms-3 fw-bold click">
-                        <i class="fa-solid fa-angle-right me-1"></i> Contact Us
-                    </div>
-                </div>
-            </div>
-
-            <a href="#"><i class="fa-solid fa-angle-up"></i></a>
-        </div>
-    </div>
+    <FooterTop />
+    <FooterLow />
   </section>
 </template>
 
 <script>
-export default {
+import FooterTop from './FooterTop.vue';
+import FooterLow from './FooterLow.vue';
 
+export default {
+    name: 'MainWeb',
+    
+    components: {
+    FooterTop,
+    FooterLow,
+  }
 }
 </script>
 
@@ -117,85 +38,4 @@ export default {
     top: 20px;
 }
 
-#footer_top{
-    background-color: $bg_footer_top;
-    color: $soft_gray;
-    font-size: 12px;
-
-    .title{
-        font-weight: bold;
-        margin-bottom: 20px;
-        color: #fff;
-    }
-
-    .subtitle{
-        font-size: 10px;
-        color: $footer_grey;
-    }
-
-    img{
-        width: 50px;
-        height: 50px;
-        border-radius: 100%;
-        object-fit: cover;
-    }
-
-    span{
-        &.active{
-            color: $blue_primary;
-            cursor: pointer;
-        }
-
-        &.tags{
-            font-size: 9px;
-            padding: 2px 6px;
-            border-radius: 5px;
-            background-color: #000;
-            color: #fff;
-            font-weight: bold;
-            cursor: pointer;
-            margin-bottom: 6px;
-            margin-right: 7px;
-        }
-    }
-
-    i{
-        &.active{
-            color: $blue_primary;
-        }
-    }
-
-    .click{
-        cursor: pointer;
-    }
-}
-
-#footer_low{
-    background-color: $bg_footer_low;
-    font-size: 10px;
-    color: $grey_nav;
-
-    .container{
-        position: relative;
-    }
-
-    img{
-        width: 15%;
-        opacity: .5;
-    }
-
-    a{
-        background-color: #444;
-        color: #fff;
-        font-size: 9px;
-        padding: 8px 13px;
-        position: absolute;
-        right: -140px;
-        bottom: 0;
-    }
-
-    .click{
-        cursor: pointer;
-    }
-}
 </style>
